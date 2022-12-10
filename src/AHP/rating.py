305b04b1721b4b16ed9adbfbd3c980a9ebc8c4ce
Adam <adam.p.kulczycki@gmail.com>
2022-12-10 20:17:49 +0100

    Added ahp rating implementation

--- src/AHP/rating.py
+++ src/AHP/rating.py
@@ -1,12 +1,32 @@
 from typing import List
 import numpy as np
+import sympy as sp
+
+
+def calc_priority_vector(matrix: np.matrix) -> np.array:
+    size = matrix.shape[0]
+    for i in range(size):
+        matrix[:, i] /= matrix[:, i].sum()
+
+    ans = np.array([0] * size, dtype=float)
+
+    for i in range(size):
+        ans[i] = matrix[i, :].sum() / float(size)
+    return ans
 
 
 def rate(criteria_matrices: List[np.matrix], criterion_importance_m: np.matrix):
     criterions = len(criteria_matrices)
     alternatives = len(criteria_matrices[0])
 
+    eigen_v: List[np.array] = []
+
     for matrix in criteria_matrices:
         if matrix.shape != (alternatives, alternatives):
             raise ValueError(f"criteria matrices must be of shape {(alternatives, alternatives)}")
 
+    for matrix in criteria_matrices:
+        eigen_v.append(calc_priority_vector(matrix))
+    priority_vectors_matrix = np.matrix(eigen_v).transpose()
+
+    return priority_vectors_matrix @ calc_priority_vector(criterion_importance_m)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
+import numpy as np
+
+from src.AHP.rating import rate
 
 
 def main():
     print("hello")
+    print(rate([np.matrix([[1, 1 / 7, 1 / 5],
+                     [7, 1, 3],
+                     [5, 1 / 3, 1]]),
+          np.matrix([[1, 1 / 3, 5],
+                     [3, 1, 7],
+                     [1 / 5, 1 / 7, 1]])],
+         np.matrix([[1, 1 / 2],
+                    [2, 1]], dtype=float)))
 
 
 if __name__ == "__main__":

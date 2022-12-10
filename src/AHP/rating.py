7c9fb5f7d0884c88639ae427bffb5a3df8c684a0
jchyczynski <jasiek.ch16@gmail.com>
2022-12-10 21:24:54 +0100

    calc_priority_vector side effects fix, koczkodaj inconsistency indices implemented

--- src/AHP/rating.py
+++ src/AHP/rating.py
@@ -1,9 +1,12 @@
+from copy import deepcopy
+from math import inf
 from typing import List
 import numpy as np
 import sympy as sp
 
 
-def calc_priority_vector(matrix: np.matrix) -> np.array:
+def calc_priority_vector(org_matrix: np.matrix) -> np.array:
+    matrix = deepcopy(org_matrix)
     size = matrix.shape[0]
     for i in range(size):
         matrix[:, i] /= matrix[:, i].sum()
@@ -16,7 +19,7 @@ def calc_priority_vector(matrix: np.matrix) -> np.array:
 
 
 def rate(criteria_matrices: List[np.matrix], criterion_importance_m: np.matrix):
-    criterions = len(criteria_matrices)
+    criteria = len(criteria_matrices)
     alternatives = len(criteria_matrices[0])
 
     eigen_v: List[np.array] = []
@@ -25,8 +28,28 @@ def rate(criteria_matrices: List[np.matrix], criterion_importance_m: np.matrix):
         if matrix.shape != (alternatives, alternatives):
             raise ValueError(f"criteria matrices must be of shape {(alternatives, alternatives)}")
 
+    if criterion_importance_m.shape != (criteria, criteria):
+        raise ValueError(f"criterion importance matrix must be of shape {(criteria, criteria)}")
+
     for matrix in criteria_matrices:
         eigen_v.append(calc_priority_vector(matrix))
     priority_vectors_matrix = np.matrix(eigen_v).transpose()
 
     return priority_vectors_matrix @ calc_priority_vector(criterion_importance_m)
+
+
+def triad_koczkodaj(i: int, j: int, k: int, pc: np.matrix) -> float:
+    print(pc)
+    print(abs(1 - pc[i, k] * pc[k, j] / pc[i, k]), abs(1 - pc[i, j] / (pc[i, k] * pc[k, j])))
+    return min(abs(1 - pc[i, k] * pc[k, j] / pc[i, k]), abs(1 - pc[i, j] / (pc[i, k] * pc[k, j])))
+
+
+def get_koczkodaj_index(pairwise_comparison: np.matrix) -> float:
+    koczkodaj = -inf
+    n = pairwise_comparison.shape[0]
+    for i in range(n):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                koczkodaj = max(koczkodaj, triad_koczkodaj(i, j, k, pairwise_comparison))
+
+    return koczkodaj
--- src/main.py
+++ src/main.py
@@ -1,19 +1,27 @@
 import numpy as np
 
-from src.AHP.rating import rate
+from src.AHP.rating import rate, get_koczkodaj_index
 
 
 def main():
-    print("hello")
-    print(rate([np.matrix([[1, 1 / 7, 1 / 5],
-                     [7, 1, 3],
-                     [5, 1 / 3, 1]]),
-          np.matrix([[1, 1 / 3, 5],
-                     [3, 1, 7],
-                     [1 / 5, 1 / 7, 1]])],
-         np.matrix([[1, 1 / 2],
-                    [2, 1]], dtype=float)))
+    pairwise_comps = [np.matrix([[1,  1/7,  1/5],
+                                 [7,  1,    3],
+                                 [5,  1/3,  1]]),
+                      np.matrix([[1, 1 / 3, 5],
+                                 [3, 1, 7],
+                                 [1 / 5, 1 / 7, 1]])]
+    # pairwise_comps = [np.matrix([[1,  10,  10],
+    #                              [10.1,  1,  10],
+    #                              [10.1,  10.1,  1]], dtype=float),
+    #                   np.matrix([[1, 1 / 3, 5],
+    #                              [3, 1, 7],
+    #                              [1 / 5, 1 / 7, 1]])]
 
+    print(rate(pairwise_comps,
+        np.matrix([[1, 1 / 2],
+                   [2, 1]], dtype=float)))
+
+    print([get_koczkodaj_index(pc) for pc in pairwise_comps])
 
 if __name__ == "__main__":
     main()

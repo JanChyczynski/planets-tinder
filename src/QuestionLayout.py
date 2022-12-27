9a56a3e655ff6980145a4dd72717ce2bc96240bb
Adam <adam.p.kulczycki@gmail.com>
2022-12-27 18:46:04 +0100

    QuestionLayout moved from main

---
+++ src/QuestionLayout.py
@@ -0,0 +1,20 @@
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+
+class QuestionLayout(BoxLayout):
+    criterion = StringProperty()
+    left_planet_name = StringProperty()
+    right_planet_name = StringProperty()
+
+    def __init__(self, criterion: str, left_planet_name: str, right_planet_name: str, **kwargs):
+        super().__init__(**kwargs)
+        self.left_planet_name = left_planet_name
+        self.right_planet_name = right_planet_name
+        self.criterion = criterion
+
+    def finish_input(self):
+        print(self.ids.comp_input_field.text)
+
+
+Builder.load_file('layouts/questionLayout.kv')
--- src/main.py
+++ src/main.py
@@ -47,24 +47,7 @@ class ListEditingLayout(BoxLayout):
         self.title = title
 
 
-class QuestionLayout(BoxLayout):
-    criterion = StringProperty()
-    left_planet_name = StringProperty()
-    right_planet_name = StringProperty()
-
-    def __init__(self, criterion: str, left_planet_name: str, right_planet_name: str, **kwargs):
-        super().__init__(**kwargs)
-        self.left_planet_name = left_planet_name
-        self.right_planet_name = right_planet_name
-        self.criterion = criterion
-
-    def finish_input(self):
-        print("test")
-        print(self.ids.comp_input_field.text)
-
-
 Builder.load_file('layouts/listEditingLayout.kv')
-Builder.load_file('layouts/questionLayout.kv')
 
 
 class MyApp(MDApp):
@@ -73,7 +56,7 @@ class MyApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "LightBlue"
         self.theme_cls.material_style = "M3"
-        return QuestionLayout("hehe","hiszpańska dziewczyna", "losowa kobieta z karczmy")
+        #return QuestionLayout("hehe", "hiszpańska dziewczyna", "losowa kobieta z karczmy")
 
 
 def main():
